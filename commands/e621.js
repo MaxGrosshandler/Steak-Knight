e5a3883f73ad2a286719c9207822ee8c11c89b6b
flap <27020481+TemTemmie@users.noreply.github.com>
2018-08-21 14:25:47 +0100

    ayy

---
+++ commands/e621.js
@@ -0,0 +1,11 @@
+module.exports = {
+msg.channel.createMessage("prankd");
+  }
+  },
+  options: {
+    description: "furries lmao",
+    fullDescription: "haha",
+    usage: "`sk e621`"
+  },
+  name: "ban"
+};
