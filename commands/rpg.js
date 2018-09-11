6d3c694243374d25bac1a50c34f91154008a2b4f
Max Grosshandler <maxgrosshandler@gmail.com>
2018-09-11 16:47:41 -0500

    testing rpg checks

--- commands/rpg.js
+++ commands/rpg.js
@@ -18,22 +18,25 @@ module.exports = {
         snark[1] = 1234
         snark[2] = Math.ceil(p.rows[0].level / 2)
         snark[3] = msg.author.id
-        snark[4] = 10 + 5 * Math.ceil(p.rows[0].level / 2)
+        snark[4] = 15 + 5 * Math.ceil(p.rows[0].level / 2)
         snark[5] = 1  * Math.ceil(p.rows[0].level / 2)
         })
        
         
-        
-        client.query("SELECT * FROM monsters where player_id = $1",spoop).then(result => {
-            if (typeof result.rows[0] == "undefined"){
+
+            client.query("SELECT * FROM monsters where player_id = $1",spoop).then(result => {
+                if (typeof snark[2] == "undefined"){
+                    msg.channel.createMessage({embed:{description:"You haven't started your adventure yet! Use `sk rpg`"}})
+                }
+            else if (typeof result.rows[0] == "undefined"){
                 client.query("INSERT INTO monsters (monster_name, monster_id, monster_level, player_id, hp,  atk ) values ($1, $2, $3, $4, $5, $6)", snark)
-                msg.channel.createMessage({embed:{description:"You found a "+snark[0] +"! It is level "+snark[2]+", has " + snark[4]+ " health, and has an attack of 1d3+" + snark[5]+ "."}})
+                msg.channel.createMessage({embed:{description:"You found a "+snark[0] +"! It is level "+snark[2]+", has " + snark[4]+ " health, and has an attack of 2d3+" + snark[5]+ "."}})
             }
             else {
                 client.query("SELECT * FROM monsters where player_id = $1",spoop).then(result => {
                 msg.channel.createMessage({embed:{description:"You are currently fighting a level "
                 +result.rows[0].monster_level+ " " + result.rows[0].monster_name +
-                 " with " +result.rows[0].hp+" hp, and  an attack of 1d3+"+result.rows[0].atk+"."}})
+                 " with " +result.rows[0].hp+" hp, and  an attack of 2d3+"+result.rows[0].atk+"."}})
             })
             }
         
@@ -48,8 +51,8 @@ module.exports = {
             else {
                 client.query("SELECT * FROM players where id = $1",[msg.author.id]).then(player =>
                      {
-                let playerHit = droll.roll(`1d6+${player.rows[0].atk}`).total;
-                let monsterHit = droll.roll(`1d3+${monster.rows[0].atk}`).total;
+                let playerHit = droll.roll(`2d6+${player.rows[0].atk}`).total;
+                let monsterHit = droll.roll(`2d3+${monster.rows[0].atk}`).total;
                 let atkDesc = "You dealt the monster " + playerHit + " damage!\n"+
                 "The monster dealt you " + monsterHit + " damage!"
                 if ( 0 >= monster.rows[0].hp - playerHit){
@@ -109,7 +112,7 @@ module.exports = {
         client.query("SELECT * FROM players where id = $1",spoop).then(result => {
             if (typeof result.rows[0] == "undefined"){
                 client.query("INSERT INTO players (id, level, hp, atk, xp, next_level, maxHP) values ($1, $2, $3, $4, $5, $6, $7)", snark)
-                msg.channel.createMessage({embed:{description:"You are level 1, have 50 hp, and have an attack of 1d6+1. You haven't done anything yet, so you have 0xp."}})
+                msg.channel.createMessage({embed:{description:"You are level 1, have 50 hp, and have an attack of 2d6+1. You haven't done anything yet, so you have 0xp."}})
             }
             else {
                 msg.channel.createMessage({embed:{description:"You are level "+result.rows[0].level+", have "+result.rows[0].hp+" out of " +result.rows[0].maxhp + " hp, and have an attack of 1d6+"+result.rows[0].atk + ".\n"
