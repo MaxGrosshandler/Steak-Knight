13ae6eb4fadbfd0b29e0d2224c443e6bf61e9ae9
Max Grosshandler <maxgrosshandler@gmail.com>
2018-09-11 20:52:42 -0500

    cat fixed the thingo

--- ZombieWatch.js
+++ ZombieWatch.js
@@ -1,18 +1,20 @@
 const channel = '132632676225122304';
-
 const color = Math.floor(Math.random() * 0xFFFFFF);
 
 module.exports = class ZombieWatch {
     constructor(bot) {
         this.bot = bot;
+        this.bot.once('ready', this.initialize.bind(this));
+    }
+
+    async initialize() {
         this.send({
             title: `Greetings!`,
-            description: `My name is 0x${this.color}, and I will be your Zombie Watcher for this evening.`
+            description: `My name is **0x${this.color}**, and I will be your Zombie Watcher for this evening.`
         });
 
         this.interval = setInterval(this.report.bind(this), 1000 * 60 * 10); // every 10 minutes, adjust as needed
     }
-
     get color() {
         return color.toString(16).toUpperCase();
     }
