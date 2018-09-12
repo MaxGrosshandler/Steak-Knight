c49bc7e50a9d416fd037e0e084ffe0a983fd2a6d
Max Grosshandler <maxgrosshandler@gmail.com>
2018-09-11 19:40:16 -0500

    further adjustment to schema and added steaks as a reward for killing a monster

--- commands/rpg.js
+++ commands/rpg.js
@@ -32,16 +32,18 @@ module.exports = {
        
         
 
-            client.query("SELECT * FROM monsters where player_id = $1",spoop).then(result => {
-                    if (typeof result.rows[0] == "undefined"){
+            client.query("SELECT * FROM monsters where player_id = $1",spoop).then(m => {
+                let monster = m.rows[0];
+                    if (typeof monster == "undefined"){
                 client.query("INSERT INTO monsters (monster_name, monster_id, monster_level, player_id, hp,  atk ) values ($1, $2, $3, $4, $5, $6)", snark)
                 msg.channel.createMessage({embed:{description:"You found a "+snark[0] +"! It is level "+snark[2]+", has " + snark[4]+ " health, and has an attack of 2d3+" + snark[5]+ "."}})
             }
             else {
-                client.query("SELECT * FROM monsters where player_id = $1",spoop).then(result => {
+                client.query("SELECT * FROM monsters where player_id = $1",spoop).then(m => {
+                    let monster = m.rows[0];
                 msg.channel.createMessage({embed:{description:"You are currently fighting a level "
-                +result.rows[0].monster_level+ " " + result.rows[0].monster_name +
-                 " with " +result.rows[0].hp+" hp, and  an attack of 2d3+"+result.rows[0].atk+"."}})
+                +monster.monster_level+ " " + monster.monster_name +
+                 " with " +monster.hp+" hp, and  an attack of 2d3+"+monster.atk+"."}})
             })
             }
         
@@ -54,37 +56,40 @@ module.exports = {
         if (guildcd.has(msg.channel.guild.id)) {
             msg.channel.createMessage("This command is on cooldown!");
     } else {
-        client.query("SELECT * FROM monsters where player_id = $1",[msg.author.id]).then(monster=> {
-            if (typeof monster.rows[0] == "undefined"){
+        client.query("SELECT * FROM monsters where player_id = $1",[msg.author.id]).then(m=> {
+            let monster = m.rows[0];
+            if (typeof monster == "undefined"){
                 msg.channel.createMessage({embed:{description:"You haven't found a monster yet! Use `sk rpg find`"}})
                 return;
             }
             else {
-                client.query("SELECT * FROM players where player_id = $1",[msg.author.id]).then(player =>
+                client.query("SELECT * FROM players where player_id = $1",[msg.author.id]).then(p =>
                      {
-                let playerHit = droll.roll(`2d6+${player.rows[0].player_atk}`).total;
-                let monsterHit = droll.roll(`2d3+${monster.rows[0].atk}`).total;
+                         let player = p.rows[0]
+                let playerHit = droll.roll(`2d6+${player.player_atk}`).total;
+                let monsterHit = droll.roll(`2d3+${monster.atk}`).total;
                 let atkDesc = "You dealt the monster " + playerHit + " damage!\n"+
                 "The monster dealt you " + monsterHit + " damage!"
-                if ( 0 >= monster.rows[0].hp - playerHit){
-                    atkDesc = "You killed the monster! Hooray! You gained " + monster.rows[0].monster_level * 20 + " xp!"
-                    client.query("DELETE FROM monsters where player_id = $1",[player.rows[0].player_id]);
-                    if (player.rows[0].player_xp + monster.rows[0].monster_level*20 >= player.rows[0].player_next_level){
-                        client.query("UPDATE players SET (xp, hp, atk, level, next_level, maxhp) = (players.xp + $1, players.maxhp + 10 , players.atk + 1, players.level+1, players.next_level + 100, players.maxhp+10)   where id = $2", [monster.rows[0].monster_level * 20, player.rows[0].id]);
-                   atkDesc += "\nAlso, you leveled up! You are now level " + (player.rows[0].player_level + 1)
+                if ( 0 >= monster.hp - playerHit){
+                    atkDesc = "You killed the monster! Hooray! You gained " + monster.monster_level * 20 + " xp! You also gained "+monster.monster_level*5+" <:steak:481449443204530197>!"
+                    client.query("INSERT INTO currency (id, money) values ($1, $2) ON CONFLICT (id) DO UPDATE SET money = currency.money + $2 WHERE currency.id = $1", [player.player_id, monster.monster_level*5])
+                    client.query("DELETE FROM monsters where player_id = $1",[player.player_id]);
+                    if (player.player_xp + monster.monster_level*20 >= player.player_next_level){
+                        client.query("UPDATE players SET (xp, hp, atk, level, next_level, maxhp) = (players.xp + $1, players.maxhp + 10 , players.atk + 1, players.level+1, players.next_level + 100, players.maxhp+10)   where id = $2", [monster.monster_level * 20, player.id]);
+                   atkDesc += "\nAlso, you leveled up! You are now level " + (player.player_level + 1)
                     }
                     else{
-                        client.query("UPDATE players SET xp = players.xp + $1 where id = $2", [monster.rows[0].monster_level * 20, player.rows[0].player_id]);
+                        client.query("UPDATE players SET xp = players.xp + $1 where id = $2", [monster.monster_level * 20, player.player_id]);
                     }
                 }
-                else if (0 >= player.rows[0].player_hp - monsterHit){
+                else if (0 >= player.player_hp - monsterHit){
                     atkDesc = "Oh no, you were killed by the monster! You'll have to find another one to fight!"
-                    client.query("DELETE FROM monsters  where player_id = $1", [player.rows[0].player_id]);
-                    client.query("UPDATE players SET hp = $1 where id = $2", [player.rows[0].player_maxhp, player.rows[0].player_id]);
+                    client.query("DELETE FROM monsters  where player_id = $1", [player.player_id]);
+                    client.query("UPDATE players SET hp = $1 where id = $2", [player.player_maxhp, player.player_id]);
                 }
                 else {
-                    client.query("UPDATE players SET hp = players.hp - $1 where id = $2",[monsterHit, player.rows[0].player_id]);
-                    client.query("UPDATE monsters SET hp = monsters.hp - $1 where player_id = $2",[playerHit, player.rows[0].player_id]);
+                    client.query("UPDATE players SET hp = players.hp - $1 where id = $2",[monsterHit, player.player_id]);
+                    client.query("UPDATE monsters SET hp = monsters.hp - $1 where player_id = $2",[playerHit, player.player_id]);
                 }
                 
 
@@ -129,7 +134,7 @@ module.exports = {
         snark[5] = 100
         snark[6] = 50
         client.query("SELECT * FROM players where id = $1",spoop).then(result => {
-            let player = result.rows[0];
+            let player = monster;
             if (typeof player == "undefined"){
                 client.query("INSERT INTO players (player_id, player_level, player_hp, player_atk, player_xp, player_next_level, player_maxhp) values ($1, $2, $3, $4, $5, $6, $7)", snark)
                 msg.channel.createMessage({embed:{description:"You are level 1, have 50 hp, and have an attack of 2d6+1. You haven't done anything yet, so you have 0xp."}})
