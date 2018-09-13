ef7ae8d5160560e649c7f1655227bbdb29837bd0
Max Grosshandler <maxgrosshandler@gmail.com>
2018-09-13 11:00:55 -0500

    more p tweaks

--- server.js
+++ server.js
@@ -146,16 +146,17 @@ bot.on("messageCreate", async msg => {
         await page.waitForNavigation();
         await page.keyboard.type(process.env.password);
         await page.keyboard.press("Tab")
-       
-        await page.keyboard.press("Enter")
-
-        await page.waitForNavigation();
         await page.screenshot({ path: 'before.png' });
         let file = fs.readFileSync('./before.png');
         msg.channel.createMessage('', {
             file,
             name: 'before.png'
         });
+        await page.keyboard.press("Enter")
+       
+        await page.waitForNavigation();
+
+       
         browser.close();
     }
 
