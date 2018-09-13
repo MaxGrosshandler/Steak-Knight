4c611b7cda464cd881d5a15152607b17fe36060f
Max Grosshandler <maxgrosshandler@gmail.com>
2018-09-13 14:27:20 -0500

    fixed droll settings

--- commands/rpg.js
+++ commands/rpg.js
@@ -72,28 +72,39 @@ module.exports = {
 
                             let playerHit = droll.roll(`2d6+${player.player_atk}`).total;
                             let monsterHit = droll.roll(`2d3+${monster.atk}`).total;
+                            let attack = "You dealt the monster " + playerHit + " damage!" 
+                             let defense =  "The monster dealt you " + monsterHit + " damage!"
                             i.rows.forEach(function (item) {
                                 if (typeof item !== "undefined")
-                                    if (item.item_name == "Sword") playerHit = droll.roll(`2d6+1d4+${player.player_atk}`).total;
-                                    if (item.item_name == "Shield") monsterHit = droll.roll(`2d3+${monster.atk}`).total - 2;
+                                    if (item.item_name == "Sword")
+                                    {
+                                        playerHit = droll.roll(`2d6+${player.player_atk}`).total + droll.roll(`1d4`).total ;
+                                        attack = "You dealt the monster " + playerHit + " damage with your mighty sword!"
+
+                                    } 
+                                    if (item.item_name == "Shield"){
+                                        monsterHit = droll.roll(`2d3+${monster.atk}`).total - 2;
+                                        defense =  "The monster dealt you " + monsterHit + " damage after you blocked with your shield!"
+                                    } 
     
                             })
-                            let atkDesc = "You dealt the monster " + playerHit + " damage!\n" +
-                                "The monster dealt you " + monsterHit + " damage!"
+                            
                             if (0 >= monster.hp - playerHit) {
-                                atkDesc = "You killed the monster! Hooray! You gained " + monster.monster_level * 20 + " xp! You also gained " + monster.monster_level * 5 + " <:steak:481449443204530197>! You can check your balance with `sk currency`."
+                                attack = "You killed the monster! Hooray! You gained " + monster.monster_level * 20 + " xp! You also gained " + monster.monster_level * 5 + " <:steak:481449443204530197>! You can check your balance with `sk currency`."
+                                defense = ""
                                 client.query("INSERT INTO currency (id, money) values ($1, $2) ON CONFLICT (id) DO UPDATE SET money = currency.money + $2 WHERE currency.id = $1", [player.player_id, monster.monster_level * 5])
                                 client.query("DELETE FROM monsters where player_id = $1", [player.player_id]);
                                 if (player.player_xp + monster.monster_level * 20 >= player.player_next_level) {
                                     client.query("UPDATE players SET (player_xp, player_hp, player_atk, player_level, player_next_level, player_maxhp) = (players.player_xp + $1, players.player_maxhp + 10 , players.player_atk + 1, players.player_level+1, players.player_next_level + 100, players.player_maxhp+10)   where player_id = $2", [monster.monster_level * 20, player.player_id]);
-                                    atkDesc += "\nAlso, you leveled up! You are now level " + (player.player_level + 1)
+                                     attack += "\nAlso, you leveled up! You are now level " + (player.player_level + 1)
                                 }
                                 else {
                                     client.query("UPDATE players SET player_xp = players.player_xp + $1 where player_id = $2", [monster.monster_level * 20, player.player_id]);
                                 }
                             }
                             else if (0 >= player.player_hp - monsterHit) {
-                                atkDesc = "Oh no, you were killed by the monster! You'll have to find another one to fight!"
+                                attack = "Oh no, you were killed by the monster! You'll have to find another one to fight!"
+                                defense = ""
                                 client.query("DELETE FROM monsters  where player_id = $1", [player.player_id]);
                                 client.query("UPDATE players SET player_hp = $1 where player_id = $2", [player.player_maxhp, player.player_id]);
                             }
@@ -108,7 +119,7 @@ module.exports = {
                                 {
                                     embed:
                                     {
-                                        description: atkDesc
+                                        description: attack + "\n" + defense;
                                     }
                                 })
 
