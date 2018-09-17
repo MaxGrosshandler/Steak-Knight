3bb23ccd100110a0aead87076a8ff1af46a4ef6a
stupid cat <cat@blargbot.xyz>
2018-09-16 23:04:13 -0600

    stupidcat command

---
+++ commands/stupidcat.js
@@ -0,0 +1,14 @@
+const serv = require("../server.js");
+const bot = serv.bot;
+module.exports = {
+  func: async (msg, args) => {
+    msg.channel.createMessage(`stupid cat is awful garbage`);
+  },
+
+  options: {
+    description: "Tells you about stupid cat",
+    fullDescription: "Tells you about stupid cat",
+    usage: "`sk stupidcat`"
+  },
+  name: "stupidcat"
+};
