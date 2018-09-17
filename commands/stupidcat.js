3cc774924aef9e9a76e41532a9e23e4d0861050c
byParallax <doyouwanttomakeacakewithme@gmail.com>
2018-09-17 10:49:29 +0200

    Update stupidcat.js

--- commands/stupidcat.js
+++ commands/stupidcat.js
@@ -2,7 +2,7 @@ const serv = require("../server.js");
 const bot = serv.bot;
 module.exports = {
   func: async (msg, args) => {
-    msg.channel.createMessage(`stupid cat is awful garbage`);
+    msg.channel.createMessage(`stupid cat is pretty cute`);
   },
 
   options: {
